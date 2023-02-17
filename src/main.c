1f9b2e17dc58f51af3ae297897b28689df3e6ad6
datwaft <datwaftx@gmail.com>
2023-02-16 20:54:49 -0600

    feat: support for getting system calls

--- src/main.c
+++ src/main.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>      // For printf
+#include <stdlib.h>     // For wait(), WIFSTOPPED(), WSTOPSIG(), WIFEXITED(),
+                        //   WIFSIGNALED(), SIGTRAP, exit()
+#include <sys/ptrace.h> // For ptrace()
+#include <unistd.h>     // For fork(), excl()
 
-int main(int argc, char **argv) {
-  puts("Hello, World!");
+int main(void) {
+  pid_t child = fork();
+  if (child == 0) {
+    ptrace(PT_TRACE_ME, 0, NULL, 0);
+    execl("/bin/ls", "ls", NULL);
+  } else {
+    int status;
+    while (1) {
+      wait(&status);
+      if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
+        printf("The child has executed a system call.\n");
+      }
+      if (WIFEXITED(status) || WIFSIGNALED(status)) {
+        printf("The child has exited or terminated.\n");
+        break;
+      }
+      ptrace(PT_CONTINUE, child, (caddr_t)1, 0);
+    }
+  }
+
   exit(0);
 }
