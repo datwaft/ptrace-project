5d6b6fdcc1894678c1a6e5edf747bd1555fed0d7
datwaft <datwaftx@gmail.com>
2023-02-19 22:27:32 -0600

    feat: use switch statement

--- src/main.c
+++ src/main.c
@@ -1,30 +1,39 @@
 #include <stdio.h>      // For printf
 #include <stdlib.h>     // For wait(), WIFSTOPPED(), WSTOPSIG(), WIFEXITED(),
                         //   WIFSIGNALED(), SIGTRAP, exit()
+#include <mach/mach.h>  // For task_for_pid() and mach_task_self()
 #include <sys/ptrace.h> // For ptrace()
 #include <sys/types.h>  // For ptrace()
 #include <sys/wait.h>   // For wait() in Linux
-#include <unistd.h>     // For fork(), excl()
+#include <unistd.h>     // For fork(), excl(), execvp()
 
-int main(void) {
-  pid_t child = fork();
-  if (child == 0) {
+int main(int argc, char *argv[]) {
+  pid_t pid = fork();
+
+  switch (pid) {
+  // On error
+  case -1: {
+    perror("fork");
+    exit(1);
+  }
+  // On child process
+  case 0: {
     ptrace(PT_TRACE_ME, 0, NULL, 0);
-    execl("/bin/ls", "ls", NULL);
-  } else {
+    execvp(argv[1], argv + 1);
+  }
+  // On parent process
+  default: {
     int status;
     while (1) {
       wait(&status);
       if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
         printf("The child has executed a system call.\n");
+        ptrace(PT_CONTINUE, pid, (caddr_t)1, 0);
       }
       if (WIFEXITED(status) || WIFSIGNALED(status)) {
-        printf("The child has exited or terminated.\n");
-        break;
+        exit(0);
       }
-      ptrace(PT_CONTINUE, child, (caddr_t)1, 0);
     }
   }
-
-  exit(0);
+  }
 }
