b0ba7f08ef1d8a70ca4db55d390dd1e4f57fd13f
datwaft <datwaftx@gmail.com>
2023-02-21 21:15:15 -0600

    feat: display register information

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
-#include <stdio.h>      // For printf
-#include <stdlib.h>     // For wait(), WIFSTOPPED(), WSTOPSIG(), WIFEXITED(),
-                        //   WIFSIGNALED(), exit()
-#include <signal.h>     // For SIGTRAP
-#include <sys/ptrace.h> // For ptrace()
-#include <sys/types.h>  // For ptrace()
-#include <sys/wait.h>   // For wait()
-#include <unistd.h>     // For fork(), excl(), execvp()
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
+  bool verbose = true;
+  bool pause = false;
+
   pid_t pid = fork();
 
   switch (pid) {
@@ -18,7 +24,7 @@ int main(int argc, char *argv[]) {
   }
   // On child process
   case 0: {
-    ptrace(PT_TRACE_ME, 0, NULL, 0);
+    ptrace(PTRACE_TRACEME);
     execvp(argv[1], argv + 1);
   }
   // On parent process
@@ -27,8 +33,39 @@ int main(int argc, char *argv[]) {
     while (1) {
       wait(&status);
       if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
-        printf("The child has executed a system call.\n");
-        ptrace(PT_CONTINUE, pid, (void *)1, 0);
+        // Obtain the registers
+        struct user_regs_struct regs;
+        ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+        if (verbose) {
+          // Obtain timestamp
+          time_t t = time(NULL);
+          struct tm *tm = localtime(&t);
+          char timestamp[64];
+          strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", tm);
+          // Print information regarding the system call
+          printf("\x1b[2m" // Dim
+                 "[%s]"
+                 "\x1b[0m" // Reset
+                 " system call "
+                 "\x1b[1;32m" // Green bold
+                 "%llu"
+                 "\x1b[0m" // Reset
+                 " called with { "
+                 "%%rdi: %llu, "
+                 "%%rsi: %llu, "
+                 "%%rdx: %llu, "
+                 "%%r10: %llu, "
+                 "%%r8: %llu, "
+                 "%%r9: %llu "
+                 "}.\n",
+                 timestamp, regs.orig_rax, regs.rdi, regs.rsi, regs.rdx,
+                 regs.r10, regs.r8, regs.r9);
+        }
+        // Continue with the child process
+        if (pause) {
+          getchar();
+        }
+        ptrace(PTRACE_CONT, pid, NULL, 0);
       }
       if (WIFEXITED(status) || WIFSIGNALED(status)) {
         exit(0);
