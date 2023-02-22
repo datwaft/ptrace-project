fe9a0a42f2c33e4f2c1b11dea59835fc512628dc
datwaft <datwaftx@gmail.com>
2023-02-21 22:40:09 -0600

    refactor: use #define for style

--- src/main.c
+++ src/main.c
@@ -11,6 +11,12 @@
 #include <time.h>
 #include <unistd.h>
 
+#define RESET "\x1b[0m"
+#define BOLD "\x1b[1m"
+#define DIM "\x1b[2m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+
 // This snippet was inspired by https://stackoverflow.com/a/24830768/10702981
 char getchar_without_echo(void) {
   struct termios old;
@@ -30,44 +36,13 @@ void print_system_call(struct user_regs_struct regs) {
   char timestamp[64];
   strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", tm);
   // Print information regarding the system call
-  printf("\x1b[2m" // Dim
-         "[%s]"
-         "\x1b[0m" // Reset
-         " system call "
-         "\x1b[34m" // Blue bold
-         "%lld"
-         "\x1b[0m" // Reset
-         " called with { "
-         "%%rdi: "
-         "\x1b[32m" // Green
-         "%#llx"
-         "\x1b[0m" // Reset
-         ", "
-         "%%rsi: "
-         "\x1b[32m" // Green
-         "%#llx"
-         "\x1b[0m" // Reset
-         ", "
-         "%%rdx: "
-         "\x1b[32m" // Green
-         "%#llx"
-         "\x1b[0m" // Reset
-         ", "
-         "%%r10: "
-         "\x1b[32m" // Green
-         "%#llx"
-         "\x1b[0m" // Reset
-         ", "
-         "%%r8: "
-         "\x1b[32m" // Green
-         "%#llx"
-         "\x1b[0m" // Reset
-         ", "
-         "%%r9: "
-         "\x1b[32m" // Green
-         "%#llx"
-         "\x1b[0m" // Reset
-         " }.\n",
+  printf(DIM "[%s]" RESET " system call " BLUE "%lld" RESET " called with { "
+             "%%rdi: " GREEN "%#llx" RESET ", "
+             "%%rsi: " GREEN "%#llx" RESET ", "
+             "%%rdx: " GREEN "%#llx" RESET ", "
+             "%%r10: " GREEN "%#llx" RESET ", "
+             "%%r8: " GREEN "%#llx" RESET ", "
+             "%%r9: " GREEN "%#llx" RESET " }.\n",
          timestamp, regs.orig_rax, regs.rdi, regs.rsi, regs.rdx, regs.r10,
          regs.r8, regs.r9);
 }
@@ -91,41 +66,34 @@ int main(int argc, char *argv[]) {
   }
   // On parent process
   default: {
-    int status;
     bool in_call = false;
     int counter = 0;
-    while (true) {
+
+    int status;
+    wait(&status);
+    while (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
+      struct user_regs_struct regs;
+      ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+      if (!in_call) {
+        if (verbose) {
+          print_system_call(regs);
+        }
+        in_call = true;
+        counter += 1;
+      } else {
+        in_call = false;
+      }
+      if (pause) {
+        getchar_without_echo();
+      }
+      ptrace(PTRACE_SYSCALL, pid, NULL, 0);
       wait(&status);
-      while (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP) {
-        struct user_regs_struct regs;
-        ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-        if (!in_call) {
-          if (verbose) {
-            print_system_call(regs);
-          }
-          in_call = true;
-          counter += 1;
-        } else {
-          in_call = false;
-        }
-        if (pause) {
-          getchar_without_echo();
-        }
-        ptrace(PTRACE_SYSCALL, pid, NULL, 0);
-        wait(&status);
-      }
-      if (WIFEXITED(status) || WIFSIGNALED(status)) {
-        printf("\x1b[2m" // Dim
-               "[RESULT]"
-               "\x1b[0m" // Reset
-               " The total number of system calls was "
-               "\x1b[1m" // Bold
-               "%d"
-               "\x1b[0m" // Reset
-               ".\n",
-               counter);
-        exit(0);
-      }
+    }
+    if (WIFEXITED(status) || WIFSIGNALED(status)) {
+      printf(DIM "[RESULT]" RESET " The total number of system calls was " BOLD
+                 "%d" RESET ".\n",
+             counter);
+      exit(0);
     }
   }
   }
